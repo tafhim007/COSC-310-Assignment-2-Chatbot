22a21f2768beeb83f3fc95eb5e2f87506374e7dd
Vaibhav R. Kumar <vaibhavk667@gmail.com>
2020-03-11 20:48:33 -0700

    Update Class1.java

--- cosc310-chatbot/src/Class1.java
+++ cosc310-chatbot/src/Class1.java
@@ -12,9 +12,11 @@ public class Class1 {
 	static int userAge;
 	static String doesSmoke;
 	static String userEmotion;
+	static String isExcercise;
 	static int numExcercise;
 	static int sleepHours;
 	static String continueChat;
+	static String seekedCounselling;
 	static String userRecommendation; //final
 	static String userRecommendationTwo;
 	static String [] greetArray = new String []{"hi", "hi there", "hello", "good morning", "good evening",
@@ -65,7 +67,7 @@ public class Class1 {
 		
 		public static void startConversation(String name) {
 			//main conversation starts here => happy guy
-			System.out.println("How do you feel today, " + name + " (Happy/Sad)?");			
+			System.out.println("How do you feel today, " + name + " (Happy/Sad)?");	
 			happyOrSad = s.nextLine();
 			happyOrSad.toLowerCase();
 			checkEmotion(happyOrSad);
@@ -74,18 +76,31 @@ public class Class1 {
 		
 		public static void checkEmotion(String userInput2) {
 			if(happyOrSad.contains("happy")) {
-				System.out.println("Glad you are happy");
-				ageQuestion();				
+				System.out.println("Glad you are happy today.");
+				counselling();				
 			}
 			else if(happyOrSad.contains("sad")){
 				System.out.println("Oh no");
-				ageQuestion();
+				counselling();
 			}
 			else {
 				defaultResponse();
 				startConversation(userName);
 			}			
 			
+		}
+		
+		public static void counselling() {
+			System.out.println("Have you seeked counselling before?");
+			seekedCounselling = s.nextLine();
+			seekedCounselling.toLowerCase();
+			if(seekedCounselling.contains("yes")) {
+				ageQuestion();
+			}
+			else {
+				System.out.println("No problem");
+				ageQuestion();
+			}
 		}
 
 		public static void ageQuestion() {
@@ -119,57 +134,84 @@ public class Class1 {
 			if(userInput.contains("yes")) {
 				System.out.println("Okay.");
 				System.out.println("Smoking only aggreviates your depression. But that's no problem.");								
-				getExcercise();
+				drink();
 			}								
 			else {
 				System.out.println("That's great to hear.");
 				System.out.println("Smoking is not healthy.");
-				getExcercise();
+				drink();
 				
 			}
 
 		}
+		public static void drink() {
+			System.out.println("Do you drink?");
+			userInput = s.next();
+			if(userInput.contains("yes")) {
+				System.out.println("Okay.");
+				System.out.println("Drinking only aggreviates your depression. But that's no problem.");								
+				getExcercise();
+			}								
+			else {
+				System.out.println("That's great to hear.");
+				System.out.println("Alcohol is the leading cause of depression.");
+				getExcercise();
+				
+			}
+
+		}				
 		
-		public static void getExcercise() {						
-			System.out.println("How many times a week do you get excercise? Enter a number");			
-			numExcercise = s.nextInt();
+		//Do you feel healthy?
+		public static void getExcercise() {		
+			System.out.println("Do you excercise? (Y/N)");
+			isExcercise = s.next();
+			isExcercise.toLowerCase();
 			
-			if(numExcercise< 3) {
-				System.out.println("Try excercising at least 3 times a week.");
-				sleepHours();
+			if(isExcercise.contains("yes")) {				
+				System.out.println("How many times a week do you get excercise? Enter a number");			
+				numExcercise = s.nextInt();
+				if(numExcercise< 3) {
+					System.out.println("Try excercising at least 3 times a week.");
+					sleepHours();
+				}
+				else {
+					System.out.println("Good job!");
+					sleepHours();
+				}
 			}
 			else {
-				System.out.println("Good job!");
-				sleepHours();
-			}
+				System.out.println("Excercise is vital to your mental health. Try excercising over weekends.");
+			}		
+			
 		}
 		
 		public static void sleepHours() {						
-			System.out.println("How many hours a week do you sleep? Enter a number");			
+			System.out.println("How many hours a day do you sleep? Enter a number");			
 			sleepHours = s.nextInt();
 			
 			if(sleepHours > 6) {
-				System.out.println("Great! You are getting good amount of sleep.");
-			}
-			else if(sleepHours<=4) {
-				System.out.println("Son you need to sleep more. At least six hours minimum");
-			}
+				System.out.println("Great! You are getting good amount of sleep.");		
+				recommendation();
+			}			
 			else {
-				System.out.println("Are you suffering from insomnia?");
+				System.out.println("Not sleeping for 16 hours straight decreases your performence as much as if your blood "
+						+ "alchohol level were 0.05.");
+				System.out.println("The legal limit is 0.08%");
+				recommendation();
 			}
 		}
 		
-		public static void recommendation() {						
+		public static void recommendation() {
+			System.out.println("I have a few questions for you.");
 			System.out.println("Do you think I have been helpful?");
 			userRecommendation = s.next();
-			System.out.println("Sure. One final question");
+			System.out.println("Sure");
 			System.out.println("I am still in training, is there any recommendation you have for me?");
 			userRecommendationTwo = s.next();
-			System.out.println("Thankyou! Your response has been recorded.");
+			System.out.println("Would you recommend this chatbot to a friend?");
+			userRecommendationTwo = s.next();
+			System.out.println("Thanks for your review. Appreciate your feedback.");
 		}
 
-		public static void sadConvo() {
-			//sid's part
 			
-		}		
 }
