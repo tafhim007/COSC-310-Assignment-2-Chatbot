73fab7c042add061c1e477ef4adfb650bed53816
Vaibhav R. Kumar <vaibhavk667@gmail.com>
2020-04-04 14:58:45 -0700

    delete trial package

--- cosc310-chatbot/src/trial/Person.java
+++
@@ -1,7 +0,0 @@
-package trial;
-
-public class Person {
-	public static void definition() {
-		System.out.println("A person is a human with 2 hands and legs.");
-	}
-}
--- cosc310-chatbot/src/trial/Student.java
+++
@@ -1,7 +0,0 @@
-package trial;
-
-public class Student {
-	public static void definiton() {
-		System.out.println("A student is someone who goes to uni or school");
-	}
-}
