a75a4b9d4b9b55e6ccdeb7909eb8fcb012f2208d
tafhim007 <43193960+tafhim007@users.noreply.github.com>
2020-04-05 23:32:57 -0700

    Update gui.java

--- cosc310-chatbot/src/gui.java
+++ cosc310-chatbot/src/gui.java
@@ -15,18 +15,26 @@ import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.ImageIcon;
 
-public class gui extends JFrame {
+public class gui extends JFrame implements ActionListener {
 
+	static ServerSocket ss;
+	static Socket s;
+	static DataInputStream dataIn;
+	static DataOutputStream dataOut;
 	
-	JPanel contentPane = new JPanel();
-	JTextArea input = new JTextArea();
-	JTextArea chatbox = new JTextArea();
+	static JPanel contentPane = new JPanel();
+	static JTextArea input = new JTextArea("");
+	static JTextArea chatbox = new JTextArea("");
 
 
 	public static void main(String[] args) {
@@ -40,6 +48,24 @@ public class gui extends JFrame {
 				}
 			}
 		});
+		
+		String msgIn = "";
+		try {
+			ss = new ServerSocket(1201);
+			s = ss.accept();
+			
+			dataIn = new DataInputStream(s.getInputStream());
+			dataOut = new DataOutputStream(s.getOutputStream());
+			
+			while(!msgIn.equals("exit")) {
+				msgIn = dataIn.readUTF();
+				
+				chatbox.setText(chatbox.getText().trim()+"\n"+msgIn);
+			}
+			
+		}catch(Exception e) {
+			System.out.println("Error");
+		}
 	}
 
 	public gui() {
@@ -70,10 +96,14 @@ public class gui extends JFrame {
 		lblTheratalk.setBounds(19, 6, 231, 59);
 		contentPane.add(lblTheratalk);
 		
+		setVisible(true);
 	}
 	
 	public void keyReleased(KeyEvent e){
 	
+		if(e.getKeyCode()==KeyEvent.VK_ENTER){
+			input.setEditable(true);
+		}
 	}
 	
 	public void keyTyped(KeyEvent e){}
@@ -84,7 +114,38 @@ public class gui extends JFrame {
 		
 	public void keyPressed(KeyEvent e){
 		if(e.getKeyCode()==KeyEvent.VK_ENTER){
+			input.setEditable(false);
 			
+			//get message
+			String quote=input.getText();
+			input.setText("");
+			addText("-->You:\t"+quote);
+			quote.trim();
+			
+			GreetUser greetClass = new GreetUser();
+			greetClass.startGreetings();
+			
+			addText("\n");
 		}
 	}
+	
+	private void btnSendAction(java.awt.event.ActionEvent evt) {
+		try {
+			String msgOut = "";
+			msgOut = input.getText().trim();
+			dataOut.writeUTF(msgOut); //sending server message
+		}catch(Exception e) {
+			System.out.println("Error");
+		}
+		
+	}
+	
+	public class txtInputListener implements ActionListener
+	{
+	    public void actionPerformed(ActionEvent event)
+	    {
+	        GreetUser.userName = input.getText();   //receive input from text field
+	        System.out.println(input);
+	    }
+	}	
 }
